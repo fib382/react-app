f83741c24437c3f2568822747ca91e173834bbc1
YK <k.yamashita.2346@gmail.com>
2021-12-14 06:34:29 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import Button from '@mui/material/Button';
+// import logo from './logo.svg';
+// import Button from '@mui/material/Button';
 import './App.css';
+import {
+  BrowserRouter, Routes, Route,
+  //  Outlet, Link 
+} from "react-router-dom";
 
-import SignIn from './components/SignIn';
+import { SignIn, SignUp } from './components/index';
+// import { Router } from '@mui/icons-material';
 
 function App() {
   return (
-    <SignIn />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -27,8 +27,7 @@ function Copyright(props) {
 }
 
 const theme = createTheme();
-
-export default function SignIn() {
+const SignIn = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
@@ -97,7 +96,7 @@ export default function SignIn() {
                                 </Link>
                             </Grid>
                             <Grid item>
-                                <Link href="#" variant="body2">
+                                <Link href="/signup" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
@@ -109,3 +108,5 @@ export default function SignIn() {
         </ThemeProvider>
     );
 }
+
+export default SignIn
